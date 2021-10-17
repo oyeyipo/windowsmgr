7c818dcbe645164430bd829e0fe0cd6f1bc614a7
Olawale Oyeyipo <olaclassik@gmail.com>
2021-10-17 16:36:30 +0100

    Feature: turn up system lit to 0 or 100

--- main.py
+++ main.py
@@ -1,36 +1,40 @@
 import sys
-import random
+import wmi
 from PySide6 import QtCore, QtWidgets, QtGui
 
 
 TURN_UP_LIGHT = "Lit Up"
+TURN_LIGHT_OUT = "Lit Out"
+
 
 class MyWidget(QtWidgets.QWidget):
 
     def __init__(self):
         super().__init__()
 
-        self.hello = ["Hallo Welt", "Hei maailma", "Hola Mundo", "Привет мир"]
-
-        self.button = QtWidgets.QPushButton(TURN_UP_LIGHT)
-
-        self.text = QtWidgets.QLabel("Hello World Mr Ola", alignment=QtCore.Qt.AlignCenter)
+        self.lit_up = QtWidgets.QPushButton(TURN_UP_LIGHT)
+        self.lit_out = QtWidgets.QPushButton(TURN_LIGHT_OUT)
 
         self.layout = QtWidgets.QVBoxLayout(self)
-        self.layout.addWidget(self.text)
-        self.layout.addWidget(self.button)
+        self.layout.addWidget(self.lit_up)
+        self.layout.addWidget(self.lit_out)
 
-        self.button.clicked.connect(self.magic)
+        self.lit_up.clicked.connect(self.light_control)
+        self.lit_out.clicked.connect(self.light_out)
 
     @QtCore.Slot()
-    def magic(self):
-        self.text.setText(random.choice(self.hello))
+    def light_control(self):
+        wmi.WMI(namespace='wmi').WmiMonitorBrightnessMethods()[0].WmiSetBrightness(100, 0)
+
+    @QtCore.Slot()
+    def light_out(self):
+        wmi.WMI(namespace='wmi').WmiMonitorBrightnessMethods()[0].WmiSetBrightness(0, 0)
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication()
 
     widget = MyWidget()
-    widget.resize(400, 300)
+    widget.resize(250, 30)
     widget.show()
 
     sys.exit(app.exec())
