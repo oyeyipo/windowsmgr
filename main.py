7580672bdfad1afad98dd6c74c49a0ec77f0044a
Olawale Oyeyipo <olaclassik@gmail.com>
2021-10-20 10:25:40 +0100

    Experimenting with switching to UI file

--- main.py
+++ main.py
@@ -38,3 +38,29 @@ if __name__ == "__main__":
     widget.show()
 
     sys.exit(app.exec())
+
+"""
+import sys
+from PySide6.QtUiTools import QUiLoader
+from PySide6.QtWidgets import QApplication
+from PySide6.QtCore import QFile, QIODevice
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+
+    ui_file_name = "test.ui"
+    ui_file = QFile(ui_file_name)
+    if not ui_file.open(QIODevice.ReadOnly):
+        print(f"Cannot open {ui_file_name}: {ui_file.errorString()}")
+        sys.exit(-1)
+    loader = QUiLoader()
+    window = loader.load(ui_file)
+    ui_file.close()
+
+    if not window:
+        print(loader.errorString())
+        sys.exit(-1)
+    window.show()
+
+    sys.exit(app.exec())
+"""
